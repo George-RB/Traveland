ac35a5e571265fb00458a15b69bac96c39c03fbc
Heorhi Kartavenko <SocialImp@mail.ru>
2025-08-31 08:44:37 +0300

    :feat: some JS

---
+++ index.js
@@ -0,0 +1,6 @@
+const bookingBtn = document.getElementById("btnBook");
+bookingBtn.addEventListener("click", function () {
+  alert(
+    "Заявка на бронирование принята! Мы свяжемся с вами в ближайшее время."
+  );
+});

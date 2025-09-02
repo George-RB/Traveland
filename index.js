320cd5223969ee831f2d3896a113e0054c84556e
Heorhi Kartavenko <SocialImp@mail.ru>
2025-09-02 09:53:20 +0300

    :feat: some JS

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const bookingBtn = document.getElementById("btnBook");
+const exploreBtn = document.getElementById("btnExplore");
+const cardTurkey = document.getElementById("cardTurkey");
+const cardMalaysia = document.getElementById("cardMalaysia");
+
+// Обработка по кнопкам в хедере
 bookingBtn.addEventListener("click", function () {
   alert(
     "Заявка на бронирование принята! Мы свяжемся с вами в ближайшее время."
   );
+});
+
+exploreBtn.addEventListener("click", function () {
+  alert("Больше информации на отдельной форме");
+});
+
+// Перенаправление по карточкам
+cardTurkey.addEventListener("click", function () {
+  const userConfirmed = confirm(
+    "Вы будете перенаправлены на сайт для заказа билетов. Перейти?"
+  );
+  if (userConfirmed) {
+    window.location.href = "https://www.tez-tour.com/";
+  } else {
+    alert("Перенаправление отменено");
+  }
+});
+
+cardMalaysia.addEventListener("click", function () {
+  const userConfirmed = confirm(
+    "Вы будете перенаправлены на сайт для заказа билетов. Перейти?"
+  );
+  if (userConfirmed) {
+    window.location.href = "https://www.malaysia.travel/";
+  } else {
+    alert("Перенаправление отменено");
+  }
 });
